bfe6d9bc897274221a509a6d829272b65b09687b
Fule <laki.linux@gmail.com>
2021-08-10 22:24:02 -0700

    fixing create new review issue

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,18 +2,18 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const car_maker = document.querySelector('input[name="post-carmaker"]').value;
-  const car_modul = document.querySelector('input[name="post-carmodel"]').value;
+  const car_model = document.querySelector('input[name="post-carmodel"]').value;
   const car_body = document.querySelector('input[name="post-carbody"]').value;
-  const post_img = document.querySelector('input[name="post-img"]').value;
+  const img_link = document.querySelector('input[name="post-img"]').value;
   const review = document.querySelector('input[name="post-review"]').value;
 
   const response = await fetch(`/api/posts`, {
     method: 'POST',
     body: JSON.stringify({
       car_maker,
-      car_modul,
+      car_model,
       car_body,
-      post_img,
+      img_link,
       review,
     }),
     headers: {
